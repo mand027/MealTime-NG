aded0ea54c69d4b16c70c0ba7301e740d343f311
Dazuam <dazuam2@gmail.com>
2019-10-22 21:12:38 -0500

    Changed dictionaries, recipe-page
    uwu

--- src/app/recipe-page/recipe-page.component.ts
+++ src/app/recipe-page/recipe-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-page',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router:Router) { }
 
   ngOnInit() {
   }
