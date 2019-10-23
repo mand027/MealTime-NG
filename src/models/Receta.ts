3dbb9164a304935d3874f0e60647f0e4e8dc7181
armando <mando27tuzo@gmail.com>
2019-10-22 20:21:45 -0500

    fix route

--- src/models/Receta.ts
+++ src/models/Receta.ts
@@ -1,5 +1,13 @@
+import { Url } from 'url';
+
 export class Receta{
     constructor(
-        public nombre?: string
+        public nombre?: string,
+        public autor?: string,
+        public descripcion?: string,
+        public ingredientes?: string,
+        public preparacion?: string,
+        public imagen?: string,
+        public video?: Url
     ){}
 }
