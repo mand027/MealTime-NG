a9d85e29a6821b52287004c7bd361c53cd7c8dd6
Armando <mando27tuzo@gmail.com>
2019-12-03 11:58:29 -0600

    sign in 80%

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -15,11 +15,36 @@ export class SignInComponent implements OnInit {
   country: string;
   city: string;
   password: string;
-  confirmpassword: string;
+
+  userModel = new Usuario;
 
   constructor(private rest: UserApiService) { }
 
   ngOnInit() {
   }
 
+
+  postUserRequest(){
+    this.userModel.personname = this.personname;
+    this.userModel.username = this.username;
+    this.userModel.email = this.email;
+    this.userModel.country = this.country;
+    this.userModel.city = this.city;
+    this.userModel.password = this.password;
+
+    console.log(this.userModel);
+
+    this.rest.postUser(this.userModel).subscribe(
+      (res: any) => {
+        console.log('HTTP response', res);
+        alert("usuario creado");
+      },
+      (err: any) => {
+        console.log('HTTP Error', err, err.status);
+        alert('No se pudo crear el usuario');
+      },
+      () => console.log('HTTP request completed.')
+    );
+  }
+  
 }
